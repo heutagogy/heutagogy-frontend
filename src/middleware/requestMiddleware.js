f6a26d57226a5ce525f95319f33d2f4f661271e4
Dmitry Rets <dmitryrets@gmail.com>
2017-01-19 21:49:36 +0100

    User fix.

--- src/middleware/requestMiddleware.js
+++ src/middleware/requestMiddleware.js
@@ -6,14 +6,17 @@ const requestMiddleware = () => () => (next) => (action) => {
   if (action[CALL_API]) {
     const headers = action[CALL_API].headers = action[CALL_API].headers || {}; // eslint-disable-line
     const method = action[CALL_API].method;
-    const token = localStorageUtils.getAuthinticatedUser().get('access_token');
+    const user = localStorageUtils.getAuthinticatedUser();
     const serverAddress = localStorageUtils.getServerInfo().get('address');
-
-    action[CALL_API].endpoint = `${serverAddress.replace(/\/$/, '')}/${action[CALL_API].endpoint}`; // eslint-disable-line
 
     if (method === 'GET' || method === 'POST' || method === 'PUT' || method === 'DELETE') {
       headers['Content-Type'] = 'application/json';
-      headers.Authorization = headers.Authorization || `JWT ${token}`;
+
+      action[CALL_API].endpoint = `${serverAddress.replace(/\/$/, '')}/${action[CALL_API].endpoint}`; // eslint-disable-line
+
+      if (user) {
+        headers.Authorization = headers.Authorization || `JWT ${user.get('access_token')}`;
+      }
 
       return next(action);
     }
