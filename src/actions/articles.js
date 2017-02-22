49d479f335e3ac9b48c888198620144e13bf8882
Alexey Shmalko <rasen.dubi@gmail.com>
2017-02-22 22:02:29 +0200

    Implement article deletion
    Problem: on mobile, there is no way to delete an article stored by
    mistake.
    
    Solution: implement article deletion on frontend (under entry menu).

--- src/actions/articles.js
+++ src/actions/articles.js
@@ -1,6 +1,6 @@
 import { CALL_API } from 'redux-api-middleware';
 import { API_VERSION } from './../constants/Api';
-import { REMEMBER_ARTICLES_VIEW_STATE, UPDATE_ARTICLE_VIEW_STATE } from './../constants/ViewStates';
+import { REMEMBER_ARTICLES_VIEW_STATE, UPDATE_ARTICLE_VIEW_STATE, DELETE_ARTICLE_VIEW_STATE } from './../constants/ViewStates';
 import schemaUtils from './../utils/schemaUtils';
 import articleSchema from './../schemas/article';
 
@@ -13,6 +13,9 @@ export const UPDATE_ARTICLE_START = 'UPDATE_ARTICLE_START';
 export const UPDATE_ARTICLE_SUCCESS = 'UPDATE_ARTICLE_SUCCESS';
 export const UPDATE_ARTICLE_FAILURE = 'UPDATE_ARTICLE_FAILURE';
 
+export const DELETE_ARTICLE_START = 'DELETE_ARTICLE_START';
+export const DELETE_ARTICLE_SUCCESS = 'DELETE_ARTICLE_SUCCESS';
+export const DELETE_ARTICLE_FAILURE = 'DELETE_ARTICLE_FAILURE';
 
 const postRememberArticles = ({ articles }) => {
   const meta = { viewId: REMEMBER_ARTICLES_VIEW_STATE, articles };
@@ -57,6 +60,25 @@ const postUpdateArticle = (articleId, articleFields) => {
 };
 
 
+const deleteDeleteArticle = (articleId) => {
+  const meta = { viewId: DELETE_ARTICLE_VIEW_STATE, articleId };
+
+  return {
+    [CALL_API]: {
+      types: [
+        { type: DELETE_ARTICLE_START, meta },
+        { type: DELETE_ARTICLE_SUCCESS, meta },
+        { type: DELETE_ARTICLE_FAILURE, meta },
+      ],
+      method: 'DELETE',
+      endpoint: `${API_VERSION}/bookmarks/${articleId}`,
+    },
+  };
+};
+
+
 export const rememberArticles = ({ articles }) => (dispatch) => dispatch(postRememberArticles({ articles }));
 
 export const updateArticle = (articleId, articleFields) => (dispatch) => dispatch(postUpdateArticle(articleId, articleFields));
+
+export const deleteArticle = (articleId) => (dispatch) => dispatch(deleteDeleteArticle(articleId));
--- src/reducers/entityReducers/articles.js
+++ src/reducers/entityReducers/articles.js
@@ -1,4 +1,4 @@
-import { UPDATE_ARTICLE_SUCCESS } from './../../actions/articles';
+import { UPDATE_ARTICLE_SUCCESS, DELETE_ARTICLE_SUCCESS } from './../../actions/articles';
 import { MINUS_ONE } from './../../constants/Constants';
 
 
@@ -15,6 +15,15 @@ export default (state, action) => {
       const newArticles = state.getIn(['article']).set(articleIndex, articlePayload);
 
       return state.setIn(['article'], newArticles);
+    }
+    case DELETE_ARTICLE_SUCCESS: {
+      const articleIndex = state.getIn(['article']).findIndex((el) => el.get('id') === action.meta.articleId);
+
+      if (articleIndex === MINUS_ONE) {
+        return state;
+      }
+
+      return state.deleteIn(['article', articleIndex]);
     }
     default: {
       return state;
