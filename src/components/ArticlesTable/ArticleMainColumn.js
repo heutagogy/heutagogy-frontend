b3190ef470593d6b70f0755df122963e40dcac0a
Dmitry Rets <dmitryrets@gmail.com>
2017-12-26 20:21:05 +0100

    Allow add/edit tags.

--- src/components/ArticlesTable/ArticleMainColumn.js
+++ src/components/ArticlesTable/ArticleMainColumn.js
@@ -1,6 +1,8 @@
-import Immutable from 'immutable';
 import { Component, PropTypes } from 'react';
 import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
+import DoneIcon from 'material-ui/svg-icons/action/done';
+import CancelIcon from 'material-ui/svg-icons/navigation/cancel';
 import { TableRowColumn } from 'material-ui/Table';
 
 import styles from './ArticlesTable.less';
@@ -13,68 +15,92 @@ const inlineStyles = {
     paddingLeft: '7px',
     paddingRight: '0',
   },
-  tags: {
-    lineHeight: '15px',
-    fontSize: '11px',
-    marginBottom: '0',
+  tag: {
+    backgroundColor: 'lightgrey',
+    border: '1px solid gray',
+    padding: '0 5px',
+    marginRight: '7px',
   },
+  tagEdit: {
+    fontSize: '14px',
+    lineHeight: '24px',
+  },
+  doneIconStyle: {
+    color: '#2196F3',
+  },
+  cancelIconStyle: {
+    color: '#f44336',
+  },
+};
+
+const initialState = {
+  isEditing: false,
+  newTitle: null,
+  newTags: [],
 };
 
 export class ArticleMainColumn extends Component {
   static propTypes = {
     read: PropTypes.string,
-    tags: PropTypes.instanceOf(Immutable.List),
+    tags: PropTypes.array,
     title: PropTypes.string,
     url: PropTypes.string,
-    onTitleChanged: PropTypes.func,
+    onArticleChanged: PropTypes.func,
   };
 
   constructor() {
     super();
 
-    this.state = {
-      isEditing: false,
-      newValue: null,
-    };
+    this.state = initialState;
 
     this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleTagsChange = this.handleTagsChange.bind(this);
     this.handleChangeComplete = this.handleChangeComplete.bind(this);
     this.handleChangeCancel = this.handleChangeCancel.bind(this);
   }
 
   handleClick(e) {
-    if (this.state.isEditing) {
+    if (this.state.isEditing === true) {
       return;
     }
 
     this.setState({
       isEditing: true,
-      newValue: this.props.title,
+      newTitle: this.props.title,
+      newTags: this.props.tags,
     });
+
     e.stopPropagation();
   }
 
-  handleChange(e, newValue) {
+  handleTitleChange(e, newTitle) {
     this.setState({
-      newValue,
+      newTitle,
+    });
+  }
+
+  handleTagsChange(e, newTags) {
+    this.setState({
+      newTags: newTags !== ''
+        ? newTags.split(',').map((s) => s.replace(/ /g, ''))
+        : [],
     });
   }
 
   handleChangeComplete(e) {
-    this.props.onTitleChanged(this.state.newValue);
-    this.setState({
-      isEditing: false,
-      newValue: null,
+    this.props.onArticleChanged({
+      title: this.state.newTitle,
+      tags: this.state.newTags,
     });
+
+    this.setState(initialState);
+
     e.preventDefault();
   }
 
   handleChangeCancel() {
-    this.setState({
-      isEditing: false,
-      newValue: null,
-    });
+    this.setState(initialState);
   }
 
   render() {
@@ -87,25 +113,59 @@ export class ArticleMainColumn extends Component {
         <div
           className={this.props.read ? styles.linkDivRead : styles.linkDivUnread}
         >
-          {this.state.isEditing
+          {this.state.isEditing === true
            ? <form onSubmit={this.handleChangeComplete}>
              <TextField
                autoFocus
                fullWidth
-               id="text-field-input"
-               value={this.state.newValue}
-               onBlur={this.handleChangeCancel}
-               onChange={this.handleChange}
+               id="title-field-input"
+               value={this.state.newTitle}
+               onChange={this.handleTitleChange}
              />
+             <TextField
+               hintText="tags"
+               id="tags-field-input"
+               style={inlineStyles.tagEdit}
+               underlineShow={false}
+               value={this.state.newTags.join(', ')}
+               onChange={this.handleTagsChange}
+             /><br />
+             <div>
+               <IconButton
+                 iconStyle={inlineStyles.doneIconStyle}
+                 type="submit"
+               >
+                 <DoneIcon />
+               </IconButton>
+               <IconButton
+                 iconStyle={inlineStyles.cancelIconStyle}
+                 style={{ float: 'right' }}
+                 onClick={this.handleChangeCancel}
+               >
+                 <CancelIcon />
+               </IconButton>
+             </div>
            </form>
-           : <a
-             href={this.props.url}
-             target="_blank"
-           >
-             {this.props.title}
-           </a>}
+           : <div>
+             <a
+               href={this.props.url}
+               target="_blank"
+             >
+               {this.props.title}
+             </a><br />
+             {
+               this.props.tags.map((tag, i) =>
+                 (
+                   <span
+                     key={`${tag}${i}`}
+                     style={inlineStyles.tag}
+                   >{tag}</span>
+                 )
+               )
+             }
+           </div>
+          }
         </div>
-        <p style={inlineStyles.tags}>{this.props.tags.toJS().join(', ')}</p>
       </TableRowColumn>);
   }
 }
