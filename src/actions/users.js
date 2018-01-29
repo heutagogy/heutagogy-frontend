f5afe2e8d3348c39e324ef879b5a68e2c6e41404
Alexey Shmalko <rasen.dubi@gmail.com>
2018-01-29 13:07:40 +0200

    Add sign in with google

--- src/actions/users.js
+++ src/actions/users.js
@@ -30,9 +30,38 @@ const fetchUserLogin = ({ login, password }) => {
   };
 };
 
+const doGoogleSignIn = ({ googleUser }) => {
+  const idToken = googleUser.getAuthResponse().id_token;
+
+  const profile = googleUser.getBasicProfile();
+  const name = profile.getName();
+
+  const meta = { viewId: LOGIN_VIEW_STATE, login: name };
+
+  return {
+    [CALL_API]: {
+      types: [
+        { type: USER_LOGIN_STARTED, meta },
+        schemaUtils.getSuccessActionTypeWithSchema({ type: USER_LOGIN_SUCCESS, schema: authUserSchema, meta }),
+        { type: USER_LOGIN_FAILURE, meta },
+      ],
+      method: 'POST',
+      body: JSON.stringify({
+        id_token: idToken,
+      }),
+      endpoint: `${API_VERSION}/login`,
+    },
+  };
+};
+
 export const loginUser = ({ login, password }) => (dispatch) => dispatch(fetchUserLogin({ login, password }));
 
+export const googleSignIn = (param) => (dispatch) => dispatch(doGoogleSignIn(param));
 
 export const USER_LOGOUT = 'USER_LOGOUT';
 
-export const logoutUser = () => ({ type: USER_LOGOUT });
+export const logoutUser = () => (dispatch) => {
+  // eslint-disable-next-line no-undef
+  gapi.auth2.getAuthInstance().signOut().then(() =>
+    dispatch({ type: USER_LOGOUT }));
+};
