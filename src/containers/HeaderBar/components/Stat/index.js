36c1e71813ff552c44b31d7ba7107b3809dd10bd
Alexey Shmalko <rasen.dubi@gmail.com>
2018-02-06 17:53:28 +0200

    Reformat stats tooltip

--- src/containers/HeaderBar/components/Stat/index.js
+++ src/containers/HeaderBar/components/Stat/index.js
@@ -2,6 +2,7 @@
 // TODO: remove eslint hack
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { connect } from 'react-redux';
+
 import IconButton from 'material-ui-next/IconButton';
 import Tooltip from 'material-ui-next/Tooltip';
 import Star from 'material-ui-icons/Star';
@@ -12,24 +13,28 @@ import type { Props, State } from './types';
 
 
 const Stat = (props: Props): React$Element<*> => {
+  const readToday = props.stat.user_read_today || 0;
+  const readYear = props.stat.user_read_year || 0;
+  const readAllTime = props.stat.user_read || 0;
+
+  const totalRead = props.stat.total_read || 0;
+  const totalRead7days = props.stat.total_read_7days || 0;
+
   const title = <p>
-    <span><b>{`Read today: ${props.stat.user_read_today || 0}`}</b></span><br />
-    <span>{`Read year: ${props.stat.user_read_year || 0}`}</span><br />
-    <span>{`Read: ${props.stat.user_read || 0}`}</span><br />
-    <span>{`Total read: ${props.stat.total_read || 0}`}</span><br />
-    <span>{`Total read (last 7 days): ${props.stat.total_read_7days || 0}`}</span><br />
+    {'Read ('}<b>{'today'}</b>{'/year/all time): '}<b>{readToday}</b>{`/${readYear}/${readAllTime}`}
+    <br />
+    {`All users (7 days/all time): ${totalRead7days}/${totalRead}`}
   </p>;
 
   const handeStarClick = props.getStatistic;
 
   return (
     <Tooltip
-      id="tooltip-bottom-start"
       title={title}
     >
       <IconButton onClick={handeStarClick} >
         <Star
-          color={(props.stat.user_read_today || 0) > 0 ? 'blue' : null}
+          color={readToday > 0 ? 'blue' : null}
         />
       </IconButton>
     </Tooltip>
